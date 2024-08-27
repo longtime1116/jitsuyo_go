94f64c55ea44a1f0d70a99e126a6cd11b64cd8a2
longtime1116 <longtime1116@gmail.com>
2024-08-28 02:51:09 +0900

    chapter3 in progress

--- chapter3/main.go
+++ chapter3/main.go
@@ -1,10 +1,38 @@
 package main
 
-import "github.com/longtime1116/jitsuyo_go/u"
+import (
+	"fmt"
+
+	"github.com/longtime1116/jitsuyo_go/u"
+)
 
 type Person struct {
 	Name string
 	Age  int
+}
+type Student struct {
+	Person
+	School string
+}
+
+func (p Person) Hello() {
+	fmt.Printf("Hello, I'm %v.\n", p.Name)
+}
+func (s Student) Hello2() {
+	// s.Name のように、Personを経由しなくても呼び出せる
+	fmt.Printf("Hello, I'm %v. I'm a student at %v.\n", s.Name, s.School)
+}
+
+func String[T any](s T) string {
+	return fmt.Sprintf("%v", s)
+}
+
+type Struct struct {
+	t interface{}
+}
+
+func (s Struct) String() string {
+	return fmt.Sprintf("%v", s.t)
 }
 
 func main() {
@@ -17,4 +45,15 @@ func main() {
 	u.N(me)
 	u.N(me2)
 
+	s := Struct{"hoge"}
+	u.P(String(s))
+	u.P(s.String())
+
+	s1 := Student{
+		Person: Person{Name: "Tom", Age: 20},
+		School: "MIT",
+	}
+	fmt.Println(s1)
+	s1.Hello() // Person のメソッドも呼び出せる
+	s1.Hello2()
 }
