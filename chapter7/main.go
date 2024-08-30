51c97dc16dd102b5229a73712a671a333b1a1d72
longtime1116 <longtime1116@gmail.com>
2024-08-31 00:40:06 +0900

    chapter7 in progress

--- chapter7/main.go
+++ chapter7/main.go
@@ -1,26 +1,7 @@
-package u
+package main
 
-import (
-	"fmt"
+func main() {
 
-	secret "github.com/longtime1116/jitsuyo_go/u/internal"
-)
-
-func P(a ...any) (n int, err error) {
-	return fmt.Println(a...)
-}
-
-func Pf(format string, a ...any) (n int, err error) {
-	return fmt.Printf(format+"\n", a...)
-}
-
-// internal の確認用(chapter6)
-func PublicSecretP(a ...any) (n int, err error) {
-	return secret.SecretP(a...)
-}
-
-// 変数未利用によるコンパイルエラー回避用
-func N(a ...any) {
 }
 
 // cmt+shift+p をして、 go interface stubs とかやって出てきたやつを使うと、stub関数を作ってくれる
