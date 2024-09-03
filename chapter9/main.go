54c8f56415c282df7dd7f2b0000c58ee8a9bf9c8
longtime1116 <longtime1116@gmail.com>
2024-09-04 01:25:49 +0900

    chapter10 in progress

--- chapter9/main.go
+++ chapter9/main.go
@@ -30,4 +30,50 @@ func main() {
 	// 	return
 	// }
 	// // 関数の最後でdeferによるRollbackが呼ばれる。トランザクションが確定していたら、このRollbackは呼ばれるものの意味は持たない
+
+	// // プリペアードステートメントで効率的に大量のデータをinsert
+	// // プリペアードステートメントとは？
+	// //    プリペアードステートメントは、SQLクエリを事前に準備し、後で複数回にわたって異なるパラメータで実行する場合に非常に便利
+	// // 		これにより、SQLインジェクション攻撃のリスクを減らし、また効率的にクエリを実行することができる。
+	// //    具体的には、以下の二点で効率的になる。
+	// //      DB側でクエリをparseし最適な実行計画をplanningする必要があるが、それが1回で済むのでCPU効率が良い
+	// //      アプリケーション側から変数の部分のデータだけを送ることになるので、1回あたりのネットワークI/Oの負荷が減る。
+	// //    命令としては、以下のような感じ。
+	// //      PREPARE stmt FROM 'INSERT INTO users (name, age) VALUES (?, ?)';
+	// //        EXECUTE stmt USING 'Alice', 30;
+	// //        EXECUTE stmt USING 'Bob', 20;
+	// //        ...
+	// //
+	// // バッチインサートとの使い分けはざっくり、以下
+	// //   少量のデータを逐次挿入したい時、リアルタイム性が要求される時などは、プリペアードステートメントを検討。
+	// //   より大規模なデータを一括で挿入したかったり、リアルタイムでなくバッチ処理が可能な場合は、バッチインサートを検討。
+	// // なお、postgres の COPY のような組み込みの関数を使うという手段もある。
+	// users := []struct {
+	// 	UserId int
+	// 	Name   string
+	// }{
+	// 	{1, "Ataro"},
+	// 	{2, "Btaro"},
+	// 	{3, "Ctaro"},
+	// }
+	// db, err := sql.Open()
+	// ctx := context.Background()
+	// tx, err := db.BeginTx(ctx, nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// stmt, err := tx.PrepareContext(ctx, "INERT INTO users(user_id, user_name)")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer stmt.Close()
+
+	// for _, u := range users {
+	// 	if _, err := tx.ExecContext(ctx, u.UserId, u.Name); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
+	// if err := tx.Commit(); err != nil {
+	// 	log.Fatal(err)
+	// }
 }
